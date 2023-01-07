46ac17f9c8c5f250e94253ac0d676e171cd0ae2d
arthu <jordnhouyon@outlook.fr>
2023-01-07 01:47:07 +0100

    exo fini

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let myId = $("#div").text("Hello World");
+myId.css("font-size","45px");
+
+let link = $("p");
+link.attr("href", "https://learn.jquery.com/using-jquery-core/attributes/");
+link.text("Lien vers jquery");
+link.css("font-size", "30px");
+
+
+let myDiv = $("#id");
+myDiv.css({"width":"200px","height":"200px","border":"2px solid red"});
+
+
